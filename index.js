83e15e8d8ec0f0cfcfd6c76a0842a14d20c1c6ca
Varun Tadpatri <varuntadpatri@gmail.com>
2025-05-23 19:56:31 +0530

    Started working on /identify endpoint did basic value extractions and creation or record logic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-
+const Contact = require('./schemas/contactSchema')
 dotenv.config()
 
 const app = express()
@@ -15,6 +15,55 @@ mongoose.connect(process.env.MONGODB_URI)
 .catch((e)=>{
     console.error(`Error connecting to MongoDB ${e}`)
 })
+
+
+
+app.post('/identify',async (req,res)=>{
+   try{
+        const {email,phoneNumber} = req.body
+    
+        if(!email && !phoneNumber){
+            return res.status(400).json({error : 'Email and phone number required'})
+        }
+
+        const matchedContacts =  await Contact.find({
+            $or : [
+                {email : email || null},
+                {phoneNumber : phoneNumber || null}
+            ]
+        });
+
+        if (matchedContacts.length === 0){
+            
+            const newContact = await Contact.create({
+                email : email || null,
+                phoneNumber : phoneNumber || null,
+                linkedId : null,
+                linkPrecedence : 'primary'
+            })
+        
+
+
+        return res.status(200).json({
+           contact : {
+            primaryContactId: newContact._id,
+            emails: [newContact.email].filter(Boolean),
+            phoneNumbers: [newContact.phoneNumber].filter(Boolean),
+            secondaryContactIds: []
+           }
+         });
+        }
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).json({error : 'Internal Server Error'})
+
+    }
+
+
+})
+
+
 
 app.get('/',(req,res)=>{
     res.send('Hello Bitespeed')
